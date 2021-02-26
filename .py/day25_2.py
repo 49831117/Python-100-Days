bdc201456841b2e95a5abac1978b2c893504b909
49831117 <49831117@mail.nknu.edu.tw>
2021-02-26 20:39:09 +0800

    update

--- .py/day25_2.py
+++ .py/day25_2.py
@@ -1,5 +1,5 @@
 # 計算各毛色數量
-
+'''
 import pandas as pd
 
 data = pd.read_csv(".py\\2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
@@ -18,3 +18,18 @@ for i in data["Primary Fur Color"]:
 
 df = pd.DataFrame([["Gray", color_g], ["Cinnamon", color_c], ["Black", color_b]], columns = ["Primary Fur Color", "Count"])
 df.to_csv("squirrel_count.csv")
+
+
+'''
+import pandas as pd
+
+data = pd.read_csv(".py\\2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+color_g_count = len(data[data["Primary Fur Color"] == "Gray"])
+color_c_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+color_b_count = len(data[data["Primary Fur Color"] == "Black"])
+
+df = pd.DataFrame([["Gray", color_g_count], ["Cinnamon", color_c_count], ["Black", color_b_count]], columns = ["Primary Fur Color", "Count"])
+df.to_csv(".py\\squirrel_count.csv")
+
+
